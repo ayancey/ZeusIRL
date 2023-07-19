61016f4c54d457b9845992cfbd16ce8be4d6bf6b
Alex Yancey <me@alexyancey.com>
2023-07-18 22:30:50 -0700

    auto discover sadly wasnt working well

--- tems.py
+++ tems.py
@@ -1,5 +1,6 @@
 import math
 from PyPav2 import Pavlok as pav
+from playsound import playsound
 
 
 class TEMSError(Exception):
@@ -12,25 +13,21 @@ class TEMSDevice(object):
         raise NotImplementedError
 
     def vibrate(self, intensity, duration):
+        """intensity from 1 to 100, duration in seconds"""
         raise NotImplementedError
 
 
+class PiShock(TEMSDevice):
+    pass
+
 class Pavlok(TEMSDevice):
     config_options = {
-        "mac_address": "MAC Address of Pavlok",
-        "auto_discover": "Automatically discover Pavlok"
+        "mac_address": ("MAC Address of Pavlok", str),
     }
 
     def __init__(self, config):
         self.config = config
         self.pav = pav()
-
-        if "auto_discover" in self.config:
-            mac_address = self.pav.discover()
-            if not mac_address:
-                raise TEMSError("Could not auto-discover your Pavlok. Try turning off Bluetooth on your phone, or pressing a button on the Pavlok to wake it up.")
-            self.config["mac_address"] = mac_address
-            del self.config["auto_discover"]
 
         if "mac_address" in self.config:
             self.pav.connect(self.config["mac_address"])
@@ -43,3 +40,21 @@ class Pavlok(TEMSDevice):
 
     def vibrate(self, intensity, duration):
         self.pav.vibrate(math.floor(intensity / 10), duration_on=duration)
+
+
+class Dummy(TEMSDevice):
+    def __init__(self, config):
+        self.config = {}
+
+    def shock(self, intensity, duration):
+        playsound("zeus.mp3")
+
+    def vibrate(self, intensity, duration):
+        playsound("vibe.mp3")
+
+
+device_map = {
+    "PiShock": PiShock,
+    "Pavlok 3": Pavlok,
+    "Dummy Device": Dummy
+}
